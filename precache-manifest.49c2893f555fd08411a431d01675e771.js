776dea602b1f09689313cf5aae9cfc1c448f965e
JonathanMasih <j24masi@siena.edu>
2023-04-11 21:57:20 -0400

    Updates

---
+++ precache-manifest.49c2893f555fd08411a431d01675e771.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3c48348ae6c13a88f7a",
+    "url": "/static/css/main.14322b21.chunk.css"
+  },
+  {
+    "revision": "e3c48348ae6c13a88f7a",
+    "url": "/static/js/main.e3c48348.chunk.js"
+  },
+  {
+    "revision": "0f42ad21f20e3536abc0",
+    "url": "/static/js/1.0f42ad21.chunk.js"
+  },
+  {
+    "revision": "5a9c9280905acaa65220",
+    "url": "/static/js/2.5a9c9280.chunk.js"
+  },
+  {
+    "revision": "04a7b79e592687f4feae",
+    "url": "/static/js/runtime~main.04a7b79e.js"
+  },
+  {
+    "revision": "7e711862a78eaf305bed91765c36efe6",
+    "url": "/static/media/profile-picture1.7e711862.jpg"
+  },
+  {
+    "revision": "1586dc2c4de4c18f54bdad60cde56136",
+    "url": "/static/media/outdoor-picture1.1586dc2c.JPG"
+  },
+  {
+    "revision": "8ba48fead2fc0cf245f352937c5e09b8",
+    "url": "/static/media/outdoor-picture2.8ba48fea.jpg"
+  },
+  {
+    "revision": "6ba8ab0ab0083a16f67f622c8ba69561",
+    "url": "/static/media/gpt-3-picture.6ba8ab0a.png"
+  },
+  {
+    "revision": "12d65d53ed7aa724bfd7f868a8a78f7b",
+    "url": "/static/media/gpt-3-picture2.12d65d53.png"
+  },
+  {
+    "revision": "1d73d27452aba77fa4291478d1365034",
+    "url": "/static/media/EmployeManagement-picture1.1d73d274.png"
+  },
+  {
+    "revision": "5da8341ba92f3665944e32d8723d372a",
+    "url": "/static/media/EmployeManagement-picture2.5da8341b.png"
+  },
+  {
+    "revision": "6dbbfb90ef14d84302d51737690600c7",
+    "url": "/static/media/spaceinvaders-pictrue.6dbbfb90.png"
+  },
+  {
+    "revision": "97ed42f98962eae4d0a146c6719117f8",
+    "url": "/static/media/spaceinvaders-pictrue2.97ed42f9.png"
+  },
+  {
+    "revision": "ed641efb75135d5cd0604d726a6769d6",
+    "url": "/index.html"
+  }
+];
